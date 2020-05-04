8e632db537b5d87acc0e969b0b891d00110682b0
sagark23 <sagarkhandelwal23@gmail.com>
2020-05-04 16:11:22 +0100

    createTask POST api

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {Task} from "./task.model";
 
@@ -10,5 +10,14 @@ export class TasksController {
     @Get()
     getAllTasks() : Task[] {
         return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    createTask(@Body('title') title: string,
+               @Body('description') description: string
+    ): Task {
+        console.log('title', title);
+        console.log('description', description);
+        return this.taskService.createTask(title, description);
     }
 }
