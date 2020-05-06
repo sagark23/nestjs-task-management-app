34c8e30454e9712c2549a065e64854f1b5f66bc0
sagark23 <sagarkhandelwal23@gmail.com>
2020-05-06 10:18:59 +0100

    added configuration file and environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import {Logger} from "@nestjs/common";
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
+  const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
 
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application is running on port ${port}`);
 }
